e096719c74a75e306481684cf05f14d8f6968264
Nathan Otto <ottoetc@gmail.com>
2016-03-31 21:50:21 -0700

    add question delete functionality

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return questions;
+    return this.store.findAll('question');
   },
+
+  actions: {
+    destroyQuestion(question) {
+      question.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
 });
