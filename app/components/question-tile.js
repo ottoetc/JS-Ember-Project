0a9192779e15e3d1a8602f0ed8d0a9b9dd8f9e18
Nathan Otto <ottoetc@gmail.com>
2016-04-08 13:33:26 -0700

    add computed property to simplify output

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  
+  authorQuestion: Ember.computed('question.author', 'question.content', function() {
+    return this.get('question.author') + ' asks: ' + this.get('question.content');
+  })
 });
